7f1dae7b79ce3fdc2086517a2c3bc706f5c1de34
GitHub Action <action@github.com>
2023-07-01 07:38:29 +0000

    Initialize template repository proper
    Set package name to com.github.blueil.sbwikimod
    Set project name to SBWikiMod
    Set modid to sbwikimod

--- src/main/java/com/github/blueil/sbwikimod/ExampleMod.java
+++ src/main/java/com/github/blueil/sbwikimod/ExampleMod.java
@@ -1,10 +1,10 @@
-package com.example;
+package com.github.blueil.sbwikimod;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = "examplemod", version = "1.0.0")
+@Mod(modid = "sbwikimod", version = "1.0.0")
 public class ExampleMod {
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
